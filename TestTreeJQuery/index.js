55f27c780cc1f47d8d285aad45aa9e35d5f1130f
Sudipto Sen <sudip2sen@gmail.com>
2020-08-08 00:27:27 -0400

    jqTree working with static data

--- TestTreeJQuery/index.js
+++ TestTreeJQuery/index.js
@@ -1,4 +1,33 @@
-﻿//$(function () {
+﻿// !!!!! Just followed this !!!!!
+
+var data = [
+    {
+        name: 'node1',
+        children: [
+            { name: 'child1' },
+            { name: 'child2' }
+        ]
+    },
+    {
+        name: 'node2',
+        children: [
+            { name: 'child3' }
+        ]
+    }
+];
+
+
+$(function() {
+    $('#tree1').tree({
+        data: data
+    });
+});
+
+
+
+
+
+//$(function () {
 //    $("#employees").dxTreeList({
 //        dataSource: employees,
 //        rootValue: -1,
@@ -18,67 +47,66 @@
 //    });
 //});
 
-
-
-$(function () {
-    $("#employees").dxTreeList({
-        dataSource: dummyData(),
-        itemsExpr: "Accounts",
-        keyExpr: "AccountId",
-        parentIdExpr: "ClientId",
-        dataStructure: "tree",
-        columns: [
-            {
-                dataField: "Name",
-                caption: "Client"
-            },
-            "AccountId", "Name"
-        ],
-        expandedRowKeys: [1],
-        showRowLines: true,
-        showBorders: true,
-        columnAutoWidth: true
-    });
-
-
-    function getDataSource() {
-        return {
-            load: function (options) {
-                return $.ajax({
-                    url: "api/Greetings/Clients",
-                    dataType: "json",
-                    data: { parentIds: options.parentIds }
-                });
-            }
-        };
-    }
-
-    function dummyData() {
-        return {
-            "result":
-                [
-                    {
-                        "Name": "Client1",
-                        "Accounts": [
-                            {
-                                "Name": "A1"
-                            },
-                            {
-                                "Name": "A2"
-                            }
-                        ]
-                    },
-                    {
-                        "Name": "Client2",
-                        "Accounts": [
-                            {
-                                "Name": "A4"
-                            },
-                            {
-                                "Name": "A5"
-                            }
-                        ]
-                    }
-                ]
-        };
-};
+//
+//$(function () {
+//    $("#employees").dxTreeList({
+//        dataSource: dummyData(),
+//        itemsExpr: "Accounts",
+//        keyExpr: "AccountId",
+//        parentIdExpr: "ClientId",
+//        dataStructure: "tree",
+//        columns: [
+//            {
+//                dataField: "Name",
+//                caption: "Client"
+//            },
+//            "AccountId", "Name"
+//        ],
+//        expandedRowKeys: [1],
+//        showRowLines: true,
+//        showBorders: true,
+//        columnAutoWidth: true
+//    });
+//
+//
+//    function getDataSource() {
+//        return {
+//            load: function (options) {
+//                return $.ajax({
+//                    url: "api/Greetings/Clients",
+//                    dataType: "json",
+//                    data: { parentIds: options.parentIds }
+//                });
+//            }
+//        };
+//    }
+//
+//    function dummyData() {
+//        return {
+//            "result":
+//                [
+//                    {
+//                        "Name": "Client1",
+//                        "Accounts": [
+//                            {
+//                                "Name": "A1"
+//                            },
+//                            {
+//                                "Name": "A2"
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        "Name": "Client2",
+//                        "Accounts": [
+//                            {
+//                                "Name": "A4"
+//                            },
+//                            {
+//                                "Name": "A5"
+//                            }
+//                        ]
+//                    }
+//                ]
+//        };
+//};
